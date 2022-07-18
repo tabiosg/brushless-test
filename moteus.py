a8dc71ac17f218bbf506d77cc689079f6afe6aa7
Guthrie <71603173+tabiosg@users.noreply.github.com>
2022-07-18 13:22:51 -0400

    Add position control mode

--- moteus.py
+++ moteus.py
@@ -23,6 +23,7 @@ class MoteusBridge:
         # This holds the controller object
         self.c = moteus.Controller(transport=f, id=1)
         self.position = math.nan
+        self.stop_position = math.nan
         self.velocity = 0
         self.state = None
 
@@ -32,21 +33,31 @@ class MoteusBridge:
     async def __exit__(self, exc_type, exc_value, traceback):
         await self.c.set_stop()
 
-    async def _set_velocity(self, speed: double) -> None:
-        self.velocity = speed
+    def _set_velocity(self, input_vel: double) -> None:
+        self.velocity = input_vel
 
     def update(self) -> None:
         self.state = await self.c.set_position(
-            position=math.nan,
-            velocity=speed,
+            position=self.position,
+            velocity=self.velocity,
             maximum_torque=0.3,
-            stop_position=pos,
+            stop_position=self.stop_position,
             watchdog_timeout=10,
             query=True,
         )
 
     def set_velocity_control_mode(self) -> None:
         self.position = math.nan
+        self.velocity = 0
+        self.stop_position=math.nan
+
+    def set_position_control_mode(self) -> None:
+        self.position = math.nan
+        self.velocity = 0.2
+        self.stop_position=math.nan
+
+    def _set_position(self, input_pos) -> None:
+        self.stop_position = input_pos
 
     def _print_position(self) -> None:
         print("Position: ", self.state.values[moteus.Register.POSITION])
@@ -56,75 +67,74 @@ class MoteusBridge:
 
     def test_velocity(self) -> None:
         self.set_velocity_control_mode()
-        for j in range(5):
-            self.set_velocity(0.2)
-            for i in range(50):
-                self.update()
-                await asyncio.sleep(0.02)
-            self.set_velocity(0.0)
-            for i in range(50):
-                self.update()
-                await asyncio.sleep(0.02)
-            self.set_velocity(-0.2)
-            for i in range(50):
-                self.update()
-                await asyncio.sleep(0.02)
-            self.set_velocity(0.0)
-            for i in range(50):
-                self.update()
-                await asyncio.sleep(0.02)
+        velocities = [0.2, 0.0, -0.2, 0.0]
+        for i in range(5):
+            for velocity in velocities:
+                self.set_velocity(velocity)
+                for j in range(50):
+                    self.update()
+                    await asyncio.sleep(0.02)
 
     def test_velocity_and_print_position(self) -> None:
         self.set_velocity_control_mode()
-        for j in range(5):
-            self.set_velocity(0.2)
-            for i in range(50):
-                self._print_position()
-                self.update()
-                await asyncio.sleep(0.02)
-            self.set_velocity(0.0)
-            for i in range(50):
-                self._print_position()
-                self.update()
-                await asyncio.sleep(0.02)
-            self.set_velocity(-0.2)
-            for i in range(50):
-                self._print_position()
-                self.update()
-                await asyncio.sleep(0.02)
-            self.set_velocity(0.0)
-            for i in range(50):
-                self._print_position()
-                self.update()
-                await asyncio.sleep(0.02)
+        velocities = [0.2, 0.0, -0.2, 0.0]
+        for i in range(5):
+            for velocity in velocities:
+                self.set_velocity(velocity)
+                for j in range(50):
+                    self._print_position()
+                    self.update()
+                    await asyncio.sleep(0.02)
 
     def test_velocity_and_print_velocity(self) -> None:
         self.set_velocity_control_mode()
-        for j in range(5):
-            self.set_velocity(0.2)
-            for i in range(50):
-                self._print_velocity()
-                self.update()
-                await asyncio.sleep(0.02)
-            self.set_velocity(0.0)
-            for i in range(50):
-                self._print_velocity()
-                self.update()
-                await asyncio.sleep(0.02)
-            self.set_velocity(-0.2)
-            for i in range(50):
-                self._print_velocity()
-                self.update()
-                await asyncio.sleep(0.02)
-            self.set_velocity(0.0)
-            for i in range(50):
-                self._print_velocity()
-                self.update()
-                await asyncio.sleep(0.02)
+        velocities = [0.2, 0.0, -0.2, 0.0]
+        for i in range(5):
+            for velocity in velocities:
+                self.set_velocity(velocity)
+                for j in range(50):
+                    self._print_velocity()
+                    self.update()
+                    await asyncio.sleep(0.02)
+
+    def test_position(self) -> None:
+        self.set_position_control_mode()
+        positions = [0.5, 0.0, -0.5, 0.0]
+        for i in range(5):
+            for position in positions:
+                self._set_position(position)
+                for j in range(50):
+                    self.update()
+                    await asyncio.sleep(0.02)
+
+    def test_position_and_print_position(self) -> None:
+        self.set_position_control_mode()
+        positions = [0.5, 0.0, -0.5, 0.0]
+        for i in range(5):
+            for position in positions:
+                self._set_position(position)
+                for j in range(50):
+                    self._print_position()
+                    self.update()
+                    await asyncio.sleep(0.02)
+
+    def test_position_and_print_velocity(self) -> None:
+        self.set_position_control_mode()
+        positions = [0.5, 0.0, -0.5, 0.0]
+        for i in range(5):
+            for position in positions:
+                self._set_position(position)
+                for j in range(50):
+                    self._print_velocity()
+                    self.update()
+                    await asyncio.sleep(0.02)
 
 
 async def main():
     with MoteusBridge() as bridge:
+        bridge.test_position()
+        bridge.test_position_and_print_position()
+        bridge.test_position_and_print_velocity()
         bridge.test_velocity()
         bridge.test_velocity_and_print_position()
         bridge.test_velocity_and_print_velocity()
