ff7fdf33329b1f1ef51bf30c59484e95840943a7
Guthrie <71603173+tabiosg@users.noreply.github.com>
2022-07-20 08:17:22 -0400

    Set position, velocity, and torque properly

--- moteus/moteus.py
+++ moteus/moteus.py
@@ -1,52 +1,82 @@
 #!/usr/bin/env python3
 
-import asyncio
+import time
 import math
 
 import moteus
 import moteus_pi3hat
+import time
+
+from dataclasses import dataclass
+
+
+@dataclass
+class ControlData:
+    """Stores video info information by keeping track of the arguments and
+    endpoint that is needed to create jetson.utils objects.
+    :var arguments: A list of strings that is needed for the jetson.utils
+        objects' capture arguments.
+    :var current_endpoint: A string that is endpoint it is assigned to.
+    """
+
+    def __init__(self) -> None:
+        self.position = math.nan
+        self.velocity = 0.0
+        self.max_torque = 0.5
+        self.ff_torque = 0.0
+        self.kp_scale = 0.0
+        self.kd_scale = 1.0
+        self.set_velocity(0.0)
+
+    def set_position(self, input_pos) -> None:
+        self.position = input_pos
+        self.velocity = 0.0
+        self.max_torque = 0.5
+        self.ff_torque = 0.0
+        self.kp_scale = 1.0
+        self.kd_scale = 1.0
+
+    def set_velocity(self, input_vel) -> None:
+        self.position = math.nan
+        self.velocity = input_vel
+        self.max_torque = 0.5
+        self.ff_torque = 0.0
+        self.kp_scale = 0.0
+        self.kd_scale = 1.0
+
+    def set_torque(self, input_torque) -> None:
+        self.position = math.nan
+        self.velocity = 0
+        self.max_torque = abs(input_torque)
+        self.ff_torque = input_torque
+        self.kp_scale = 0.0
+        self.kd_scale = 0.0
 
 
 class Controller:
 
-    def __init__(self, transport, servo_id) -> None:
-        self.c = moteus.Controller(transport=transport, id=servo_id)
-        self.position = math.nan
-        self.stop_position = math.nan
-        self.velocity = 0
+    def __init__(self, servo_id) -> None:
+        self.c = moteus.Controller(id=servo_id)
         self.state = None
+        self.control_data = ControlData()
 
-    async def __enter__(self):
-        await self.c.set_stop()
+    def __enter__(self):
+        self.c.make_stop()
 
-    async def __exit__(self, exc_type, exc_value, traceback):
-        await self.c.set_stop()
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.c.make_stop()
 
-    def set_velocity(self, input_vel: float) -> None:
-        self.velocity = input_vel
-
-    async def update(self) -> None:
-        self.state = await self.c.set_position(
-            position=self.position,
-            velocity=self.velocity,
-            maximum_torque=0.3,
-            stop_position=self.stop_position,
+    def update(self) -> None:
+        self.state = self.c.make_position(
+            position=self.control_data.position,
+            velocity=self.control_data.velocity,
+            feedforward_torque=self.control_data.ff_torque,
+            kp_scale=self.control_data.kp_scale,
+            kd_scale=self.control_data.kd_scale,
+            maximum_torque=self.control_data.max_torque,
             watchdog_timeout=10,
             query=True,
         )
-
-    def set_velocity_control_mode(self) -> None:
-        self.position = math.nan
-        self.velocity = 0
-        self.stop_position = math.nan
-
-    def set_position_control_mode(self) -> None:
-        self.position = math.nan
-        self.velocity = 0.2
-        self.stop_position = math.nan
-
-    def set_position(self, input_pos) -> None:
-        self.stop_position = input_pos
 
     def print_position(self) -> None:
         print("Position: ", self.state.values[moteus.Register.POSITION])
@@ -65,30 +95,16 @@ class MoteusBridge:
         """
         ids = [1, 0, 0, 0, 0]  # 0 means nothing 
 
-        # If there were multiple motors connected, then
-        # f would need to be modified
-        f = moteus_pi3hat.Pi3HatRouter(
-                servo_bus_map={
-                1:[ids[0]],
-                2:[ids[1]],
-                3:[ids[2]],
-                4:[ids[3]],
-                5:[ids[4]],
-            },
-        )
-
         # This holds the controller object
         self.servos = [
-            Controller(f, ids[0]),
-            Controller(f, ids[1]),
-            Controller(f, ids[2]),
-            Controller(f, ids[3]),
-            Controller(f, ids[4]),
+            Controller(ids[0]),
+            Controller(ids[1]),
+            Controller(ids[2]),
+            Controller(ids[3]),
+            Controller(ids[4]),
         ]
 
-    async def test_velocity(self) -> None:
-        for servo in servos:
-            servo.set_velocity_control_mode()
+    def test_velocity(self) -> None:
         velocities = [0.2, 0.0, -0.2, 0.0]
         for i in range(5):
             for velocity in velocities:
@@ -97,11 +113,9 @@ class MoteusBridge:
                 for j in range(50):
                     for servo in servos:
                         servo.update()
-                    await asyncio.sleep(0.02)
+                    sleep(0.02)
 
-    async def test_velocity_and_print_position(self) -> None:
-        for servo in servos:
-            servo.set_velocity_control_mode()
+    def test_velocity_and_print_position(self) -> None:
         velocities = [0.2, 0.0, -0.2, 0.0]
         for i in range(5):
             for velocity in velocities:
@@ -111,11 +125,9 @@ class MoteusBridge:
                     for servo in servos:
                         servo.print_position()
                         servo.update()
-                    await asyncio.sleep(0.02)
+                    time.sleep(0.02)
 
-    async def test_velocity_and_print_velocity(self) -> None:
-        for servo in servos:
-            servo.set_velocity_control_mode()
+    def test_velocity_and_print_velocity(self) -> None:
         velocities = [0.2, 0.0, -0.2, 0.0]
         for i in range(5):
             for velocity in velocities:
@@ -125,11 +137,9 @@ class MoteusBridge:
                     for servo in servos:
                         servo.print_velocity()
                         servo.update()
-                    await asyncio.sleep(0.02)
+                    time.sleep(0.02)
 
-    async def test_position(self) -> None:
-        for servo in servos:
-            servo.set_position_control_mode()
+    def test_position(self) -> None:
         positions = [0.5, 0.0, -0.5, 0.0]
         for i in range(5):
             for position in positions:
@@ -138,11 +148,9 @@ class MoteusBridge:
                 for j in range(50):
                     for servo in servos:
                         servo.update()
-                    await asyncio.sleep(0.02)
+                    time.sleep(0.02)
 
-    async def test_position_and_print_position(self) -> None:
-        for servo in servos:
-            servo.set_position_control_mode()
+    def test_position_and_print_position(self) -> None:
         positions = [0.5, 0.0, -0.5, 0.0]
         for i in range(5):
             for position in positions:
@@ -152,11 +160,9 @@ class MoteusBridge:
                     for servo in servos:
                         servo.print_position()
                         servo.update()
-                    await asyncio.sleep(0.02)
+                    time.sleep(0.02)
 
-    async def test_position_and_print_velocity(self) -> None:
-        for servo in servos:
-            servo.set_position_control_mode()
+    def test_position_and_print_velocity(self) -> None:
         positions = [0.5, 0.0, -0.5, 0.0]
         for i in range(5):
             for position in positions:
@@ -166,10 +172,10 @@ class MoteusBridge:
                     for servo in servos:
                         servo.print_velocity()
                         servo.update()
-                    await asyncio.sleep(0.02)
+                    time.sleep(0.02)
 
 
-async def main():
+def main():
     with MoteusBridge() as bridge:
         bridge.test_position()
         bridge.test_position_and_print_position()
@@ -180,4 +186,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
